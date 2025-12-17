334bb592013f783c1366d6306ca35420baaec4ba
CyrillJosh <act.cjodato@gmail.com>
2025-12-17 20:47:15 +0800

    Add project files.

---
+++ Controllers/VehicleManagementController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SUPREA_LOGISTICS.Models;
+using System.Text.Json;
+using IOFile = System.IO.File;
+
+namespace SUPREA_LOGISTICS.Controllers
+{
+    public class VehicleManagementController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            string path = Path.Combine(
+                Directory.GetCurrentDirectory(),
+                "wwwroot",
+                "Vehicles.json"
+            );
+
+            // 1️⃣ Declare json
+            string json = IOFile.ReadAllText(path);
+            var vehicles = JsonSerializer.Deserialize<List<Vehicle>>(json);
+
+            return View(vehicles);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(string id)
+        {
+            string path = Path.Combine(
+                Directory.GetCurrentDirectory(),
+                "wwwroot",
+                "Vehicles.json"
+            );
+            string json = IOFile.ReadAllText(path);
+            var vehicles = JsonSerializer.Deserialize<List<Vehicle>>(json);
+            var vehicle = vehicles.FirstOrDefault(v => v.VehicleId == id);
+            if (vehicle == null)
+            {
+                return NotFound();
+            }
+            return View(vehicle);
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Vehicle.cs
@@ -0,0 +1,62 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SUPREA_LOGISTICS.Models
+{
+    public class Vehicle
+    {
+        [JsonPropertyName("VECHICLE ID")]
+        public string VehicleId { get; set; }
+
+        [JsonPropertyName("UNIT TYPE")]
+        public string UnitType { get; set; }
+
+        [JsonPropertyName("UNIT MODEL/SERIES")]
+        public string UnitModelSeries { get; set; }
+
+        [JsonPropertyName("BRAND/MAKE")]
+        public string BrandMake { get; set; }
+
+        [JsonPropertyName("YEAR MODEL")]
+        public string YearModel { get; set; }
+
+        [JsonPropertyName("ENGINE NO.")]
+        public string EngineNo { get; set; }
+
+        [JsonPropertyName("CHASIS NO.")]
+        public string ChasisNo { get; set; }
+
+        [JsonPropertyName("PLATE NO")]
+        public string PlateNo { get; set; }
+
+        [JsonPropertyName("OR/CR")]
+        public string OrCr { get; set; }
+
+        [JsonPropertyName("EXPIRATION")]
+        public string Expiration { get; set; }
+
+        [JsonPropertyName("INSURANCE ")]
+        public string Insurance { get; set; }
+
+        [JsonPropertyName("INSURANCE COVERAGE")]
+        public string InsuranceCoverage { get; set; }
+
+        [JsonPropertyName("INSURANCE PROVIDER")]
+        public string InsuranceProvider { get; set; }
+
+        [JsonPropertyName("DATE ACQUIRED")]
+        public string DateAcquired { get; set; }
+
+        [JsonPropertyName("SUPPLIER")]
+        public string Supplier { get; set; }
+
+        [JsonPropertyName("PROJECT ID")]
+        public string ProjectId { get; set; }
+
+        [JsonPropertyName("SITE LOCATION")]
+        public string SiteLocation { get; set; }
+
+        [JsonPropertyName("VEHICLE STATUS")]
+        public string VehicleStatus { get; set; }
+    }
+
+}
